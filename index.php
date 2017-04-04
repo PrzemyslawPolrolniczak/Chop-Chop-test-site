85e4ec3f6e96da259b507e5386eaab045859105b
Przemyslaw Polrolniczak <przemekp96@gmail.com>
2017-04-04 21:36:30 +0200

    made section 2 with articles

--- index.php
+++ index.php
@@ -19,9 +19,32 @@
         <span>Contact</span>
       </menu>
     </header>
-    <section>
+    <section class="hero">
       This is just a sample page
       <div class="button">push the button</div>
+    </section>
+    <section class="content">
+      <div>
+        <h1>A heading title</h1>
+        <div class="square"></div>
+        <p class="inlineParagraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis felis fermentum, dictum odio ut.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget ex nulla. Integer ullamcorper nibh lorem, ut porttitor mi sagittis eget. Aenean malesuada mattis justo, sed efficitur nisl fringilla eget. Integer a mi quis lectus vulputate tempus quis quis dui. Mauris suscipit euismod urna, eget feugiat tellus imperdiet vitae. Vestibulum.</p>
+        <div class="more"><span>-</span><span>-</span><span>-</span><span>-</span><span>-</span>&nbsp;&nbsp;read more</div>
+      </div>
+      <div>
+        <h1>A  much longer heading title</h1>
+        <div class="square"></div>
+        <p class="inlineParagraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis felis fermentum, dictum odio ut.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget ex nulla. Integer ullamcorper nibh lorem, ut porttitor mi sagittis eget. Aenean malesuada mattis justo, sed efficitur nisl fringilla eget.</p>
+        <div class="more lower"><span>-</span><span>-</span><span>-</span><span>-</span><span>-</span>&nbsp;&nbsp;read more</div>
+      </div>
+      <div>
+        <h1>A heading title</h1>
+        <div class="square"></div>
+        <p class="inlineParagraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis felis fermentum, dictum odio ut.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget ex nulla. Integer ullamcorper nibh lorem, ut porttitor mi sagittis eget. Aenean malesuada mattis justo, sed efficitur nisl fringilla eget. Integer a mi quis lectus vulputate tempus quis quis dui. Mauris suscipit euismod urna, eget feugiat tellus imperdiet vitae. Vestibulum.</p>
+        <div class="more"><span>-</span><span>-</span><span>-</span><span>-</span><span>-</span>&nbsp;&nbsp;read more</div>
+      </div>
     </section>
   </body>
 </html>
